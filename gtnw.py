3788a6757f58d99bd262150389706112342c9d19
ricfield <ricfield@gmail.com>
2018-11-29 12:41:15 -0500

    Initial framework

---
+++ gtnw.py
@@ -0,0 +1,15 @@
+def main():
+   print("\n\nWelcome to Global Thermnonuclear War!.\n\n
+   print("1. USA")
+   print("2. Russia")
+   side = input("Choose a side: ")
+   
+   if side == 1:
+      # do stuff
+   else if side == 2:
+      # do stuff
+
+if __name__ == '__main__':
+   main()
+   
+   

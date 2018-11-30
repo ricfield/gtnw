4a748b2fb4479657e18a4f012aa19c0b5e542a46
Richard A. Field <ricfield@gmail.com>
2018-11-30 16:25:32 -0500

    Logic debugging

--- City.py
+++ City.py
@@ -3,14 +3,16 @@ MAX_HIT_POINTS = 100
 
 import random
 class City:
-   def __init__(city_name):
+
+   hit_points = MAX_HIT_POINTS
+
+   def __init__(self, city_name):
       self.city_name = city_name
-      hit_points = MAX_HIT_POINTS
       random.seed()
    
    def rebuild():
       hit_points = random.randint(1, MAX_HIT_POINTS)
-      print(self.city_name + " rebuilt with " str(hit_points) + " hit points.")
+      print(self.city_name + " rebuilt with " + str(hit_points) + " hit points.")
       
    def destroy():
       hit_points = 0
--- gtnw.py
+++ gtnw.py
@@ -1,51 +1,60 @@
-import City, random
+from City import City
+import random
 
+MAX_HIT_POINTS = 100
 
 usa_cities = [ City("a"), City("b"), City("c") ]
-russia_cities = [ City("1"), City("2"), City("3") ]
-                                                    
+russia_cities = [ City("1"), City("2"), City("3") ]                                                 
 
 def main():
-   print("\n\nWelcome to Global Thermnonuclear War!.\n\n
+   
+   i = 1
+   print("\n\nWelcome to Global Thermnonuclear War!.\n\n")
    print("1. USA")
    print("2. Russia")
-   side = input("Choose a side: ")
+   side = int(input("Choose a side: "))
 
    # USA side
    if side == 1:
        print("Attack targets:")
        for city in russia_cities:
-          i = 1
           print(str(i) + ". " + city.city_name)
-          i += 1
+          i = i + 1
        
-       keyin = input("Select a target: ")
-  
-       if keyin == 1:
-          fire_missile(russia_cities[keyin])
-          russia_cities[keyin].hit_points = russia_cities[1].hit_points - fire_missile(russia_cities[keyin])
+       keyin = int(input("Select a target: "))
+       keyin -= 1  
+
+       if keyin == 0:
+          russia_cities[keyin].hit_points -= fire_missile(russia_cities[keyin])
+          
           if russia_cities[keyin].hit_points > 0:
               print(russia_cities[keyin].city_name + " was not destroyed!")
+              print(russia_cities[keyin].city_name + " has " + str(ussia_cities[keyin].hit_points) + " HP.")
           else:
               print(russia_cities[keyin].city_name + " was destroyed.")
-       else if keyin == 2:
-          fire_missile(russia_cities[keyin])
-          russia_cities[keyin].hit_points = russia_cities[1].hit_points - fire_missile(russia_cities[keyin])
-          if russia_cities[keyin].hit_points > 0:
+       
+       if keyin == 1:
+          russia_cities[keyin].hit_points -= fire_missile(russia_cities[keyin])
+          
+          if russia_cities[int(keyin)].hit_points > 0:
               print(russia_cities[keyin].city_name +  " was not destroyed!")
+              print(russia_cities[keyin].city_name + " has " + str(russia_cities[keyin].hit_points) + " HP.")
           else:
               print(russia_cities[keyin].city_name + " was destroyed.")
-       else if keyin == 3:
-          fire_missile(russia_cities[keyin])
-          russia_cities[keyin].hit_points = russia_cities[1].hit_points - fire_missile(russia_cities[keyin])
+       
+       if keyin == 2:
+          russia_cities[keyin].hit_points -= fire_missile(russia_cities[keyin])
+          
           if russia_cities[keyin].hit_points > 0:
               print(russia_cities[keyin].city_name + " was not destroyed!") 
+              print(russia_cities[keyin].city_name + " has " + str(russia_cities[keyin].hit_points) + " HP.")
+          
           else:
               print(russia_cities[keyin].city_name + " was destroyed.")
          
          
    # Russia side      
-   else if side == 2:
+   #elif side == 2:
       # do stuff
 
          
@@ -57,11 +66,12 @@ def fire_missile(city):
       
    if hit == True:
       print("Missile hit " + city.city_name)
-      hit_points_lost = hit_points_lost - random.randint(1, MAX_HIT_POINTS)
-      else:
-         print("Missile missed " + target.city_name)
-         
-    return hit_points_lost
+      hit_points_lost = random.randint(1, MAX_HIT_POINTS)
+   else:
+       print("Missile missed " + city.city_name)
+   
+   print(str(hit_points_lost))     
+   return hit_points_lost
          
          
 if __name__ == '__main__':
